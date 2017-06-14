633eb0fba8bed20ad457f7df3dcb23d61fe4e5be
Aakash Bajaj <f2015586@pilani.bits-pilani.ac.in>
2017-06-15 00:29:33 +0530

    Form Rectification

--- survey/models.py
+++ survey/models.py
@@ -17,17 +17,18 @@ class Team(models.Model):
 
 class RC_Building(models.Model):
 	# Basic Info
-	uniq = models.PositiveIntegerField("Unique ID", unique=True)
+	uniq = models.PositiveIntegerField("Unique ID", blank=True, null=True)
 	team = models.ForeignKey(Team, on_delete = models.DO_NOTHING, blank=True)
-	bl_id = models.CharField("Building ID",max_length=10, primary_key = True)
+	bl_id = models.CharField("Building ID",max_length=10, primary_key = True, blank=True)
 	addr = models.CharField("Address",max_length = 200)
 	gps_x = models.DecimalField("Latitude",max_digits = 10, decimal_places = 7)
 	gps_y = models.DecimalField("Longtitude",max_digits = 10, decimal_places = 7)
-	oc_day = models.PositiveIntegerField("Occupancy: Day")
-	oc_night = models.PositiveIntegerField("Occupancy: Night")
-	no_floor = models.PositiveSmallIntegerField("No. of Floors")
-	yr_constr = models.PositiveSmallIntegerField("Year of Construction")
-	yr_extn = models.PositiveSmallIntegerField("Year of Extension(If Any)",blank=True)
+	oc_day = models.DecimalField("Occupancy: Day", max_digits = 10, decimal_places = 0)
+	oc_night = models.DecimalField("Occupancy: Night", max_digits = 10, decimal_places =0)
+	no_floor = models.DecimalField("No. of Floors", max_digits = 2, decimal_places = 0)
+	bas_prsnt = models.BooleanField("Basement", blank = True, )
+	yr_constr = models.DecimalField("Year of Construction", max_digits = 4, decimal_places = 0)
+	yr_extn = models.DecimalField("Year of Extension(If Any)",max_digits=4, decimal_places=0, blank=True, null=True)
 	acc_level = models.CharField("Access Level",max_length = 10)
 	bl_use = models.CharField("Building Use",max_length = 50)
 	s_zone = models.PositiveIntegerField("Seismic Zone")
@@ -40,9 +41,15 @@ class RC_Building(models.Model):
 	shr_col = models.BooleanField(default = False)
 
 	def save(self, *args, **kwargs):
-		tm_cnt = Building.objects.filter(team = self.team).count() + 1
-		bl_id = self.team.name + '-' + str(tm_cnt)
-		uniq = Building.objects.all().count() + 1
+		print(self.yr_constr)
+		print(self.yr_extn)
+		print('\n')
+		print(self.yr_extn is None)
+		tm_cnt = RC_Building.objects.filter(team = self.team).count() + 1
+		print(tm_cnt)
+		self.bl_id = self.team.name + '-' + str(tm_cnt)
+		print(self.bl_id)
+		self.uniq = RC_Building.objects.all().count() + 1
 		super(RC_Building, self).save(*args, **kwargs)
 	
 	def __str__(self):
