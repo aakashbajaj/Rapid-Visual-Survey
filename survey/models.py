ac667e1cfbad07b5e47148065cb65018232d1cf9
Aakash Bajaj <f2015586@pilani.bits-pilani.ac.in>
2017-06-14 15:14:53 +0530

    Text in Models

--- survey/models.py
+++ survey/models.py
@@ -13,18 +13,18 @@ class Team(models.Model):
 
 class Building(models.Model):
 	team = models.ForeignKey(Team, on_delete = models.DO_NOTHING)
-	bl_id = models.PositiveIntegerField()
-	addr = models.CharField(max_length = 200)
-	gps_x = models.DecimalField(max_digits = 10, decimal_places = 7)
-	gps_y = models.DecimalField(max_digits = 10, decimal_places = 7)
-	oc_day = models.PositiveIntegerField()
-	oc_night = models.PositiveIntegerField()
-	no_floor = models.PositiveSmallIntegerField()
-	yr_constr = models.PositiveSmallIntegerField()
-	yr_extn = models.PositiveSmallIntegerField(blank=True)
-	acc_level = models.CharField(max_length = 10)
-	bl_use = models.CharField(max_length = 50)
-	s_zone = models.PositiveIntegerField()
+	bl_id = models.PositiveIntegerField("Building ID")
+	addr = models.CharField("Address",max_length = 200)
+	gps_x = models.DecimalField("Latitude",max_digits = 10, decimal_places = 7)
+	gps_y = models.DecimalField("Longtitude",max_digits = 10, decimal_places = 7)
+	oc_day = models.PositiveIntegerField("Occupancy: Day")
+	oc_night = models.PositiveIntegerField("Occupancy: Night")
+	no_floor = models.PositiveSmallIntegerField("No. of Floors")
+	yr_constr = models.PositiveSmallIntegerField("Year of Construction")
+	yr_extn = models.PositiveSmallIntegerField("Year of Extension(If Any)",blank=True)
+	acc_level = models.CharField("Access Level",max_length = 10)
+	bl_use = models.CharField("Building Use",max_length = 50)
+	s_zone = models.PositiveIntegerField("Seismic Zone")
 	
 	def __str__(self):
 		return self.bl_id
@@ -36,4 +36,3 @@ class RC(models.Model):
 	pl_irr = models.BooleanField(default = False)
 	hvy_ovh = models.BooleanField(default = False)
 	shr_col = models.BooleanField(default = False)
-	
