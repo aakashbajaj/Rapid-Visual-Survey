21b217b6ef6e4695dd09e00821dade3ecc88733f
Aakash Bajaj <f2015586@pilani.bits-pilani.ac.in>
2017-06-19 12:15:12 +0530

    RC Model Modified

--- survey/admin.py
+++ survey/admin.py
@@ -3,15 +3,34 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-from .models import RC_Building, Team, MS_Building, HY_Building, CM_Building
+from .models import RC_Building, Team, MS_Building, HY_Building
 # Register your models here.
 
 admin.site.register(Team)
-admin.site.register(CM_Building)
 admin.site.register(HY_Building)
 admin.site.register(MS_Building)
-admin.site.register(RC_Building)
 
+class RCAdminSet(admin.ModelAdmin):
+	fieldsets = [
+		('ID', {'fields':['uniq', 'bl_id']}),
+
+		('Basic Information', {'fields':['team','addr','no_floor','bas_prsnt','yr_constr',
+'yr_extn','acc_level','bl_use','op_bl_use','s_zone',]}),
+
+		('Occupancy', {'fields':['oc_day','oc_night']}),
+
+		('GPS Coordinates', {'fields':['gps_x','gps_y']}),
+
+		('Date and Time', {'fields':['dt_tkn']}),
+
+		('Building Features', {'fields':['soft_st','vrt_irr','pl_irr','hvy_ovh','frm_act','shr_col']}),
+		
+		('Falling Hazards', {'fields':['rf_sign','ac_grl','el_prp','hv_elf','hv_cnp','sb_bal','hv_cld','str_gl'], 'classes':['collapse']})
+	]
+
+	list_display = ('bl_id','dt_tkn')
+
+admin.site.register(RC_Building, RCAdminSet)
 # admin.site.unregister(Building)
 # admin.site.unregister(RC)
 # admin.site.unregister(Team)
--- survey/models.py
+++ survey/models.py
@@ -16,6 +16,27 @@ class Team(models.Model):
 		return self.name
 
 class RC_Building(models.Model):
+	
+	# Choices for all fields
+	ACCESS_CHOICES = (
+		('FULL', 'FULL'),
+		('PARTIAL', 'PARTIAL'),
+		('NO', 'NO')
+	)
+
+	SEISMIC_ZONE = (
+		( 1, 'II and III'),
+		( 2, 'IV'),
+		( 3, 'V')
+	)
+
+	BLD_USE = (
+		('Residential','Residential'),
+		('Commercial','Commercial'),
+		('Mixed','Mixed'),
+		('Others','Others')
+	)
+	
 	# Basic Info
 	uniq = models.PositiveIntegerField("Unique ID", blank=True, null=True)
 	team = models.ForeignKey(Team, on_delete = models.DO_NOTHING)
@@ -23,24 +44,45 @@ class RC_Building(models.Model):
 	addr = models.CharField("Address",max_length = 200)
 	gps_x = models.DecimalField("Latitude",max_digits = 10, decimal_places = 7)
 	gps_y = models.DecimalField("Longtitude",max_digits = 10, decimal_places = 7)
-	oc_day = models.DecimalField("Occupancy: Day", max_digits = 10, decimal_places = 0)
-	oc_night = models.DecimalField("Occupancy: Night", max_digits = 10, decimal_places =0)
+	oc_day = models.DecimalField("Day", max_digits = 10, decimal_places = 0)
+	oc_night = models.DecimalField("Night", max_digits = 10, decimal_places =0)
 	no_floor = models.DecimalField("No. of Floors", max_digits = 2, decimal_places = 0)
-	bas_prsnt = models.BooleanField("Basement", blank = True, )
+	bas_prsnt = models.NullBooleanField("Basement")
 	yr_constr = models.DecimalField("Year of Construction", max_digits = 4, decimal_places = 0)
-	yr_extn = models.DecimalField("Year of Extension(If Any)",max_digits=4, decimal_places=0, blank=True, null=True)
-	acc_level = models.CharField("Access Level",max_length = 10)
-	bl_use = models.CharField("Building Use",max_length = 50)
-	s_zone = models.PositiveIntegerField("Seismic Zone")
+	yr_extn = models.DecimalField("Year of Extension (If Any)",max_digits=4, decimal_places=0, blank=True, null=True)
+	bl_use = models.CharField("Building Use",max_length = 50, choices=BLD_USE)
+	op_bl_use = models.CharField("If Others, Specify",max_length=50, blank=True, null=True)
+	acc_level = models.CharField("Access Level",max_length = 10, choices=ACCESS_CHOICES)
+	s_zone = models.PositiveIntegerField("Seismic Zone", choices=SEISMIC_ZONE)
 	
+	# Date and Time Taken
+	dt_tkn = models.DateTimeField("Taken On")
+
 	# Features
 	soft_st = models.NullBooleanField("Soft Storey")
-	vrt_irr = models.BooleanField("Vertical Irregularities",default = False)
-	pl_irr = models.BooleanField("Plan Irregularities",default = False)
-	hvy_ovh = models.BooleanField("Heavy Overhangs",default = False)
-	shr_col = models.BooleanField("Short Column",default = False)
+	vrt_irr = models.NullBooleanField("Vertical Irregularities")
+	pl_irr = models.NullBooleanField("Plan Irregularities")
+	hvy_ovh = models.NullBooleanField("Heavy Overhangs")
+	shr_col = models.NullBooleanField("Short Column")
+
+	# Other Features
+	frm_act = models.NullBooleanField("Frame Action Present")
+
+	# Falling Hazards
+	rf_sign = models.NullBooleanField("Marquees/Hoardings/Roof Signs", default=False)
+	ac_grl = models.NullBooleanField("AC Units/Grillwork", default=False)
+	el_prp = models.NullBooleanField("Elaborate Parapets", default=False)
+	hv_elf = models.NullBooleanField("Heavy Elevation Features", default=False)
+	hv_cnp = models.NullBooleanField("Heavy Canopies", default=False)
+	sb_bal = models.NullBooleanField("Substantial Balconies", default=False)
+	hv_cld = models.NullBooleanField("Heavy Cladding", default=False)
+	str_gl = models.NullBooleanField("Structural Glazing", default=False)
+
+	# Signature URL
+	sign_url = models.CharField(max_length = 300)
 
 	def save(self, *args, **kwargs):
+		self.soft_st = True
 		print(self.yr_constr)
 		print(self.yr_extn)
 		print('\n')
@@ -50,10 +92,15 @@ class RC_Building(models.Model):
 		print(self.uniq is None)
 		tm_cnt = RC_Building.objects.filter(team = self.team).count() + 1
 		print(tm_cnt)
-		self.bl_id = self.team.name + '-' + str(tm_cnt)
+
+
+		if self.bl_id is None:
+			self.bl_id = self.team.name + '-' + str(tm_cnt)
 		print(self.bl_id)
-		self.uniq = RC_Building.objects.all().count() + 1
-		super(RC_Building, self).save(*args, **kwargs)
+		if self.uniq is None:
+			self.uniq = RC_Building.objects.all().count() + 1
+		
+		return super(RC_Building, self).save(*args, **kwargs)
 	
 	def __str__(self):
 		return self.bl_id
@@ -123,28 +170,3 @@ class HY_Building(models.Model):
 	class Meta:
 		verbose_name = "Hybrid Building"
 		verbose_name_plural = "Hybrid Buildings"
-
-class CM_Building(models.Model):
-	# Basic Info
-	uniq = models.PositiveIntegerField("Unique ID", blank=True, null=True)
-	team = models.ForeignKey(Team, on_delete = models.DO_NOTHING)
-	bl_id = models.CharField("Building ID",max_length=10, null=True	, blank=True)
-	addr = models.CharField("Address",max_length = 200)
-	gps_x = models.DecimalField("Latitude",max_digits = 10, decimal_places = 7)
-	gps_y = models.DecimalField("Longtitude",max_digits = 10, decimal_places = 7)
-	oc_day = models.DecimalField("Occupancy: Day", max_digits = 10, decimal_places = 0)
-	oc_night = models.DecimalField("Occupancy: Night", max_digits = 10, decimal_places =0)
-	no_floor = models.DecimalField("No. of Floors", max_digits = 2, decimal_places = 0)
-	bas_prsnt = models.BooleanField("Basement", blank = True, )
-	yr_constr = models.DecimalField("Year of Construction", max_digits = 4, decimal_places = 0)
-	yr_extn = models.DecimalField("Year of Extension(If Any)",max_digits=4, decimal_places=0, blank=True, null=True)
-	acc_level = models.CharField("Access Level",max_length = 10)
-	bl_use = models.CharField("Building Use",max_length = 50)
-	s_zone = models.PositiveIntegerField("Seismic Zone")
-
-	def __str__(self):
-		return self.bl_id
-
-	class Meta:
-		verbose_name = "Composite Building"
-		verbose_name_plural = "Composite Buildings"
