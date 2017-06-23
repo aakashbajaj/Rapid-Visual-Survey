94e87aa7c2abaa134b1344e629e6ff56badbc211
Aakash Bajaj <f2015586@pilani.bits-pilani.ac.in>
2017-06-24 04:19:41 +0530

    Admin Panel for MS

--- survey/admin.py
+++ survey/admin.py
@@ -52,7 +52,47 @@ class RCAdminSet(admin.ModelAdmin):
 	list_display = ('bl_id','dt_tkn')
 
 admin.site.register(RC_Building, RCAdminSet)
-# admin.site.unregister(Building)
-# admin.site.unregister(RC)
-# admin.site.unregister(Team)
 
+
+class MSAdminSet(admin.ModelAdmin):
+
+	fieldsets = [
+		('ID', {'fields':['uniq', 'bl_id']}),
+
+		('Date and Time', {'fields':['dt_tkn']}),
+
+		('Basic Information', {'fields':['team','addr','ty_const','no_floor','bas_prsnt','yr_constr', 'yr_aval','yr_extn','acc_level','bl_use','op_bl_use','s_zone']}),
+
+		('Occupancy', {'fields':['oc_day','oc_night','oc_navl']}),
+
+		('GPS Coordinates', {'fields':['gps_x','gps_y']}),
+
+		('Structural Irregularities', {'fields':['lck_wll', 'hvy_ovh', 're_crn', 'crn_bld']}),
+
+		('Openings', {'fields':['prt_opn', 'irr_opn', 'opn_crn']}),
+
+		('Diaphragm Action', {'fields':['ab_diap', 'lrg_cut']}),
+
+		('Horizontal Bands', {'fields':['plnt_lvl', 'lntl_lvl', 'sill_lvl', 'roof_lvl']}),
+
+		('Arches', {'fields':['arches', 'jck_roof']}),
+
+		('Apparent Quality', {'fields':['ql_mat','maintc']}),
+
+		('Random Rubble Stone Masonary Walls', {'fields':['thk_wll', 'rnd_stn', 'hvy_roof']}),
+
+		('Pounding', {'fields':['cnt_bld', 'pr_qlt']}),
+
+		('Soil Condition', {'fields':['soil_cn']}),
+
+		('Building Features', {'fields':['str_irr', 'diap_ab', 'hrz_bnd', 'arch', 'pnding', 'rub_wll']}),
+
+		('Falling Hazards', {'fields':['rf_sign','ac_grl','el_prp','hv_elf','hv_cnp','sb_bal','hv_cld','str_gl']}),
+
+		('Performance Score', {'fields':['perf_score']}),
+
+		('Signature', {'fields':['sign_url']})	
+
+	]
+
+	list_display = ('bl_id', 'dt_tkn')
